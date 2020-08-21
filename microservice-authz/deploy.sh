09c0a1f58cfb2261eb01e69440ccf3abad71f529
Anders Eknert <anders.eknert@bisnode.com>
2020-08-22 01:55:29 +0200

    Containerized app

--- microservice-authz/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kind create cluster --name opa-demo
-
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+k() {
+    kubectl --context kind-opa-demo "$@"
+}
+
+kind create cluster --name opa-demo --config kind-config.yaml
+
+# Install nginx ingress controller
+k apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+
+# Install microservices
+cd microservice-authz
+#docker build -t eknert/opa-demo-app:test .
+kind load docker-image eknert/opa-demo-app:test --name opa-demo
+
+k create configmap microservice-policy --from-file policy/policy.rego
+
+k apply -f resources.yaml
+
+# Install admission controller webhooks
+
